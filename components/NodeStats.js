792600d077ec5ad33329fbb27bdf9366640c4484
huukhoa1412 <59795495+huukhoa1412@users.noreply.github.com>
2025-12-17 21:13:20 +0700

    Update NodeStats.js

--- components/NodeStats.js
+++ components/NodeStats.js
@@ -65,9 +65,9 @@ export default function NodeStats({ nodeId }) {
         </div>
         <div className="flex flex-col items-end">
           <span className="text-gray-400 text-xs font-bold uppercase tracking-widest">Requirements</span>
-          <span className={`text-lg font-bold ${lastSync.meetsRequirements ? 'text-blue-400' : 'text-yellow-400'}`}>
-            {lastSync.meetsRequirements ? "PASSED" : "FAILED"}
-          </span>
+         // <span className={`text-lg font-bold ${lastSync.meetsRequirements ? 'text-blue-400' : 'text-yellow-400'}`}>
+         //   {lastSync.meetsRequirements ? "PASSED" : "FAILED"}
+         // </span>
         </div>
       </div>
 
